5a461901627d65ab4ecadf9968fd601768edc982
Mikel Sagardia <mxagar@gmail.com>
2022-12-01 09:21:20 +0000

    prediction function completed

--- src/serve/predict.py
+++ src/serve/predict.py
@@ -90,6 +90,10 @@ def predict_fn(input_data, model):
     # TODO: Compute the result of applying the model to the input data. The variable `result` should
     #       be a numpy array which contains a single integer which is either 1 or 0
 
-    result = model(data)
+    with torch.no_grad():
+        output = model.forward(data)
     
-    return result.cpu().numpy()
+    output = output.detach().cpu()
+    result = np.round(output.numpy())
+    
+    return result
